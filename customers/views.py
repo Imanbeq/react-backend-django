e04ceec4d09f47c4c7cdbf361aec3937baa4cc07
Qlove Iman <imanqlove@gamil.com>
2023-09-30 17:06:12 +0600

    added admin.py and customer

--- customers/views.py
+++ customers/views.py
@@ -1,9 +1,18 @@
 from customers.models import Customer
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
 from customers.serializers import CustomerSerilizer
 
 def customers(request):
     data = Customer.objects.all()
     serializer = CustomerSerilizer(data, many=True)
     return JsonResponse({'customers': serializer.data})
+    
+
+def customer(request, id):
+    try:
+        data = Customer.objects.get(pk=id)
+    except Customer.DoesNotExist:
+        raise Http404('Customer does not found')
+    serializer = CustomerSerilizer(data)
+    return JsonResponse({'customer': serializer.data})
     
